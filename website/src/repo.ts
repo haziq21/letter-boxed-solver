5a34ed5215111bbe87ef40c43a3b0d6fafffe0ad
Haziq Hairil <haziqhairil21@gmail.com>
2025-05-12 20:39:42 +0800

    Order solutions alphabetically

--- website/src/repo.ts
+++ website/src/repo.ts
@@ -3,7 +3,7 @@ import { desc } from "drizzle-orm";
 
 /** Retrieve all solutions from the database. Return a Map of date strings to arrays of solutions. */
 export async function getAllSolutions(): Promise<Map<string, string[][]>> {
-  const sols = await db.select().from(solutions).orderBy(desc(solutions.date));
+  const sols = await db.select().from(solutions).orderBy(desc(solutions.date), solutions.words);
 
   // Aggregate the solutions by date
   const solsMap = new Map<string, string[][]>();
