9d63b42c73e6a4e5640fdfa10f36a8997fbb50a7
Haziq Hairil <haziqhairil21@gmail.com>
2025-05-12 20:21:07 +0800

    Use prod credentials in prod

--- website/drizzle.config.ts
+++ website/drizzle.config.ts
@@ -1,11 +1,14 @@
 import "dotenv/config";
 import { defineConfig } from "drizzle-kit";
 
+const dbCredentials =
+  process.env.ENVIRONMENT === "production"
+    ? { url: process.env.DB_URL!, authToken: process.env.DB_AUTH_TOKEN! }
+    : { url: `file:${process.env.DB_FILE_NAME!}` };
+
 export default defineConfig({
   out: "./drizzle",
   schema: "./src/db/schema.ts",
-  dialect: "sqlite",
-  dbCredentials: {
-    url: process.env.DB_FILE_NAME!,
-  },
+  dialect: process.env.ENVIRONMENT === "production" ? "turso" : "sqlite",
+  dbCredentials,
 });
--- website/src/db/index.ts
+++ website/src/db/index.ts
@@ -4,13 +4,13 @@ import { drizzle } from "drizzle-orm/libsql";
 export let db: ReturnType<typeof drizzle>;
 export * from "./schema";
 
-if (process.env.ENVIRONMENT !== "production") {
-  db = drizzle(`file:${process.env.DB_FILE_NAME!}`);
-} else {
+if (process.env.ENVIRONMENT === "production") {
   db = drizzle({
     connection: {
       url: process.env.DB_URL!,
       authToken: process.env.DB_AUTH_TOKEN!,
     },
   });
+} else {
+  db = drizzle(`file:${process.env.DB_FILE_NAME!}`);
 }
